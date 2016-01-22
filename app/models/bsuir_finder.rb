b218425b3bfbe3f7bfec07c5592cd404df563bdb
yauhenininjia <thisislogiiiiin@gmail.com>
2016-01-22 17:52:17 +0300

    finder and sentiment analyzer implemented

--- app/models/bsuir_finder.rb
+++ app/models/bsuir_finder.rb
@@ -1,2 +1,54 @@
+require 'open-uri'
+
 class BsuirFinder < ActiveRecord::Base
+  GROUPS_URL = 'http://www.bsuir.by/schedule/rest/studentGroup'
+  SCHEDULE_URL = 'http://www.bsuir.by/schedule/rest/schedule/'
+  LECTORS_URL = 'http://bsuir-helper.ru/lectors'
+  BSUIR_HELPER_URL = 'http://bsuir-helper.ru'
+
+  def self.find_group_id(number)
+    group_id = nil
+    groups = Nokogiri::XML(open GROUPS_URL)
+    groups.xpath('//studentGroup').find { |group| group.xpath('name').text == number }
+      .xpath('id').text
+  end
+
+  def self.parse_teacher(employee)
+    { last_name: employee.xpath('lastName').text, first_name: employee.xpath('firstName').text,
+        middle_name: employee.xpath('middleName').text, comments: [] }
+  end
+
+  def self.find_teachers(group_id)
+    teachers = []
+    schedule = Nokogiri::XML(open(SCHEDULE_URL + group_id.to_s))
+    schedule.xpath('//employee').each do |employee|
+      teacher = parse_teacher employee
+      teachers << teacher
+    end
+    teachers.uniq!
+  end
+
+  def self.find_teachers_url(teacher)
+    page = Nokogiri::HTML(open LECTORS_URL)
+    references = page.xpath('//a')
+    url = nil
+    references.each do |reference|
+      url = reference.values.first if reference.text.split.join(' ') == "#{teacher[:last_name]} #{teacher[:first_name]} #{teacher[:middle_name]}"
+    end
+    url
+  end
+
+  def self.find_comments(teacher)
+    teacher_comments = []
+    url = find_teachers_url teacher
+    unless url.nil?
+      page = Nokogiri::HTML(open(BSUIR_HELPER_URL + url))
+      times = page.xpath("//div[@class='rounded-inside']/div/div[@class='submitted']/span[@class='comment-date']")
+      comments = page.xpath("//div[@class='rounded-inside']/div/div[@class='content']/p")
+      times.each_with_index do |time, index|
+        teacher_comments << { time: time.text, text: comments[index].text }
+      end
+    end
+    teacher_comments
+  end
 end
--- app/models/sentiment_analyzer.rb
+++ app/models/sentiment_analyzer.rb
@@ -1,2 +1,19 @@
+require 'net/http'
+
 class SentimentAnalyzer < ActiveRecord::Base
+  SENTIMENT_ANALYZER_URL = 'https://russiansentimentanalyzer.p.mashape.com/rsa/sentiment/polarity/json/'
+  ANALYZER_URI = URI(SENTIMENT_ANALYZER_URL)
+
+  def self.analyze(text)
+    request = Net::HTTP::Post.new(ANALYZER_URI)
+    request['X-Mashape-Key'] = '43dvDjcWRWmsh6Zf8juVR6dmjZVwp1YuHfqjsn2prBDhAPMhqK'
+    request['Content-Type'] = 'application.json'
+    request['Accept'] = 'text/plain'
+    request.body = { text: text, output_format: 'json' }.to_json
+    
+    http = Net::HTTP.new(ANALYZER_URI.hostname, ANALYZER_URI.port)
+    http.use_ssl = true
+    response = http.request(request)
+    JSON.parse(response.body)['sentiment']
+  end
 end
